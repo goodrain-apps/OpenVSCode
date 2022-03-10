3ae9a7ebc5a0641062367a7f675845f54b08798f
dazuimao1990 <33855684+dazuimao1990@users.noreply.github.com>
2022-03-10 17:15:39 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Init with extensions Gitlab-workflow
-if [ ! -d /home/workspace/.openvscode-server/extensions/gitlab.gitlab-workflow-3.40.2 ];then
+if [ ! -d /home/workspace/.openvscode-server/extensions/gitlab.gitlab-workflow-3.40.2 ] && [ ! -d /home/workspace/.openvscode-server/extensions ];then
+  mkdir -p /home/workspace/.openvscode-server/extensions/
   tar xf /home/gitlab.gitlab-workflow-3.40.2.tar -C /home/workspace/.openvscode-server/extensions/
 fi
 
